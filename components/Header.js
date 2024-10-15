c83b46a015b22f1aacb36c248b662c9f5473981f
degenscotty <scottvermast@gmail.com>
2024-10-15 12:45:54 +0200

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import { ConnectButton } from "web3uikit"
+
+export default function () {
+    return (
+        <div>
+            Decentralized Lottery
+            <ConnectButton moralisAuth={false} />
+        </div>
+    )
+}
